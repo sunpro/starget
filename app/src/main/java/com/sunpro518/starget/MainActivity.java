1086b7123420426fb1b5c277b91de4a9ad32cf0f
sunpro <sunjinshengli@163.com>
2016-11-07 11:41:36 +0800

    create small target app

---
+++ app/src/main/java/com/sunpro518/starget/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sunpro518.starget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
